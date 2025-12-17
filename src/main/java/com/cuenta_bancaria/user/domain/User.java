99842aa5164ff796ace2a88d1ff7c3e9797ee1ce
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-17 20:58:52 -0300

    Creando clase User para la capa de dominio

---
+++ src/main/java/com/cuenta_bancaria/user/domain/User.java
@@ -0,0 +1,22 @@
+package com.cuenta_bancaria.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String alias;
+    private String email;
+    private String password;
+    private boolean status;
+
+}
