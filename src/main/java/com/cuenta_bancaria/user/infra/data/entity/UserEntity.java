22fe1e56f054b4e42cfb4867e3fec73e326edb61
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-17 21:27:33 -0300

    Creando clase UserEntity en la capa de infra

---
+++ src/main/java/com/cuenta_bancaria/user/infra/data/entity/UserEntity.java
@@ -0,0 +1,37 @@
+package com.cuenta_bancaria.user.infra.data.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    private String alias;
+
+    private String email;
+
+    private String password;
+
+    private boolean status;
+
+}
