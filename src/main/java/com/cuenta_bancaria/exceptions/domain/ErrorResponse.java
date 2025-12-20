e84d9cd981c3325b1cb66ee740c8fc93014a8b98
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-20 13:59:02 -0300

    Exceptions: Creando clase ErrorResponse

---
+++ src/main/java/com/cuenta_bancaria/exceptions/domain/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.cuenta_bancaria.exceptions.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+public class ErrorResponse {
+    private String code;
+    private String message;
+    private LocalDateTime timestamp;
+    
+    private List<String> details;
+}
