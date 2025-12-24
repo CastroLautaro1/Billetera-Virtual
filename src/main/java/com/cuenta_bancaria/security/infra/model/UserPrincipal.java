9a1e1973e9af82d78c0f7c07ec866daaecdf8fac
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-23 22:54:40 -0300

    Security: Clase UserPrincipal, implementa UserDetails.

---
+++ src/main/java/com/cuenta_bancaria/security/infra/model/UserPrincipal.java
@@ -0,0 +1,67 @@
+package com.cuenta_bancaria.security.infra.model;
+
+import com.cuenta_bancaria.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String alias;
+
+    private String email;
+
+    private String password;
+
+    private User.Role role;
+
+    private boolean status;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
