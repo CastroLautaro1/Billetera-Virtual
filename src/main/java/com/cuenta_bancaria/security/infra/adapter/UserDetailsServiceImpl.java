f0ac7e555c1c31a18c18d6418395faf6dc6ea6ca
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-23 22:55:51 -0300

    Security: Creando clase que implementa la interfaz UserDetailsService.

---
+++ src/main/java/com/cuenta_bancaria/security/infra/adapter/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.cuenta_bancaria.security.infra.adapter;
+
+import com.cuenta_bancaria.security.infra.model.UserPrincipal;
+import com.cuenta_bancaria.user.domain.User;
+import com.cuenta_bancaria.user.domain.port.UserRepositoryPort;
+import com.cuenta_bancaria.user.infra.data.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepositoryPort userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No hay coincidencias con el email ingresado"));
+
+        return UserPrincipal.builder()
+                .id(user.getId())
+                .firstname(user.getFirstname())
+                .lastname(user.getLastname())
+                .alias(user.getAlias())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .status(user.isStatus())
+                .build();
+    }
+
+}
