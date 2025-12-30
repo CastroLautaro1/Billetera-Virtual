76c8f333fe3ce739c2159cc9ac109e232e7dce5d
Castro Lautaro <castrolautaro235@gmail.com>
2025-12-30 16:04:46 -0300

    refactor(transaction): agregando validaciones de persistencia a la clase TransactionEntity.

--- src/main/java/com/cuenta_bancaria/transaction/infra/data/entity/TransactionEntity.java
+++ src/main/java/com/cuenta_bancaria/transaction/infra/data/entity/TransactionEntity.java
@@ -21,16 +21,27 @@ public class TransactionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "type")
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
     private Transaction.TransactionType transactionType;
+
     @Column(name = "origin_account_id")
     private Long originAccountId;
+
     @Column(name = "counterparty_account_id")
     private Long counterpartyAccountId;
+
+    @Column(nullable = false)
     private double amount;
-    @Column(name = "resulting_balance")
+
+    @Column(name = "resulting_balance", nullable = false)
     private double resultingBalance;
+
+    @Column(nullable = false, length = 100)
     private String details;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private OffsetDateTime timestamp;
 
 
