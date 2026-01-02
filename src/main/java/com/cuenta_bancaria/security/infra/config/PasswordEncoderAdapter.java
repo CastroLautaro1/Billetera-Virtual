3c1e2596ebb18713305b7ca54d85c1d6cd6d20d5
Castro Lautaro <castrolautaro235@gmail.com>
2026-01-02 20:13:06 -0300

    feature(security): implementando puerto AuthPasswordEncoderPort en la clase PasswordEncoderAdapter.

--- src/main/java/com/cuenta_bancaria/security/infra/config/PasswordEncoderAdapter.java
+++ src/main/java/com/cuenta_bancaria/security/infra/config/PasswordEncoderAdapter.java
@@ -1,5 +1,6 @@
 package com.cuenta_bancaria.security.infra.config;
 
+import com.cuenta_bancaria.auth.domain.AuthPasswordEncoderPort;
 import com.cuenta_bancaria.user.domain.port.external.PasswordEncoderPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,12 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class PasswordEncoderAdapter implements PasswordEncoderPort {
+public class PasswordEncoderAdapter implements PasswordEncoderPort, AuthPasswordEncoderPort {
 
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public String encode(String rawPassword) {
         return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public String encodePass(String password) {
+        return passwordEncoder.encode(password);
     }
 }
