52d6cad43f4b06f29cddcf137c0cf3e39902daa4
Castro Lautaro <castrolautaro235@gmail.com>
2026-02-13 21:37:25 -0300

    feature(security): add cors configuration

---
+++ src/main/java/com/billetera_virtual/security/infra/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.billetera_virtual.security.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todos los endpoints
+                        .allowedOrigins("http://localhost:4200") // URL de tu proyecto Angular
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
