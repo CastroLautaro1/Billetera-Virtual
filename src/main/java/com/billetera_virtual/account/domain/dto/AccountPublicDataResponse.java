fdb6ce42d3d42b9233fc379a1423bbe716af559a
Castro Lautaro <castrolautaro235@gmail.com>
2026-04-02 13:16:30 -0300

    refactor: rename counterpartyId to accountId in AccountPublicDataResponse DTO

--- src/main/java/com/billetera_virtual/account/domain/dto/AccountPublicDataResponse.java
+++ src/main/java/com/billetera_virtual/account/domain/dto/AccountPublicDataResponse.java
@@ -1,7 +1,8 @@
 package com.billetera_virtual.account.domain.dto;
 
+// DTO que combina la informacion de un Usuario y su Cuenta
 public record AccountPublicDataResponse (
-        Long counterpartyId,
+        Long accountId,
         String firstname,
         String lastname,
         String alias,
