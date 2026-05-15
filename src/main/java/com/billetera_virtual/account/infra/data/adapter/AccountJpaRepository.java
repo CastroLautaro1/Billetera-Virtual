75fe5423cd8f47743ca06cb92738c492b70c6251
Castro Lautaro <castrolautaro235@gmail.com>
2026-05-15 12:26:27 -0300

    feat: add pagination support to AccountJpaRepository for active accounts

--- src/main/java/com/billetera_virtual/account/infra/data/adapter/AccountJpaRepository.java
+++ src/main/java/com/billetera_virtual/account/infra/data/adapter/AccountJpaRepository.java
@@ -1,11 +1,13 @@
 package com.billetera_virtual.account.infra.data.adapter;
 
 import com.billetera_virtual.account.infra.data.entity.AccountEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface AccountJpaRepository extends JpaRepository<AccountEntity, Long> {
@@ -14,6 +16,7 @@ public interface AccountJpaRepository extends JpaRepository<AccountEntity, Long>
     Optional<AccountEntity> findByUserId(Long userId);
     Optional<AccountEntity> findByAliasAndStatusTrue(String alias);
     Optional<AccountEntity> findByCvuAndStatusTrue(String cvu);
+    Page<AccountEntity> findByStatusTrue();
     @Modifying
     @Query("update AccountEntity a set a.status = false where a.id = :id")
     void logicallyDeleteById(@Param("id") Long id);
